13ea591956ad3f1aafdbc0c299a06ee4631ae6b6
AlexandrBxbrxv <g1026@bk.ru>
2024-10-21 16:47:50 +0500

    обновление

--- vehicle/models.py
+++ vehicle/models.py
@@ -11,3 +11,15 @@ class Car(models.Model):
     class Meta:
         verbose_name = 'машина'
         verbose_name_plural = 'машины'
+
+
+class Moto(models.Model):
+    title = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'мотоцикл'
+        verbose_name_plural = 'мотоциклы'
