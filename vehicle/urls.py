39e6c5df8bbfe05b5737d87db622ca3ba60f67ff
AlexandrBxbrxv <g1026@bk.ru>
2024-10-21 14:59:11 +0500

    обновление

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from vehicle.apps import VehicleConfig
+from vehicle.views import CarViewSet
+
+app_name = VehicleConfig.name
+
+router = DefaultRouter()
+router.register('cars', CarViewSet, basename='cars')
 
 urlpatterns = [
 
-]
+] + router.urls
