6ad836a31132c283a3cc81909a6e7e28d3528bb6
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 18:00:05 +0500

    обновлен

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from vehicle.apps import VehicleConfig
 from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
-    MotoDestroyAPIView, MilageCreateAPIView
+    MotoDestroyAPIView, MilageCreateAPIView, MotoMilageListAPIView
 
 app_name = VehicleConfig.name
 
@@ -16,6 +16,7 @@ urlpatterns = [
     path('moto/detail/<int:pk>/', MotoRetrieveAPIView.as_view(), name='moto_detail'),
     path('moto/update/<int:pk>/', MotoUpdateAPIView.as_view(), name='moto_update'),
     path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto_delete'),
+    path('moto/milage/list/', MotoMilageListAPIView.as_view(), name='moto_milage_list'),
 
     path('milage/create/', MilageCreateAPIView.as_view(), name='milage_create'),
 ] + router.urls
