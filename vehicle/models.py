d44b0d091b6aca650d12527aa9d1e0c483cf96ac
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 16:44:32 +0500

    + Milage

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Car(models.Model):
     title = models.CharField(max_length=150, verbose_name='наименование')
@@ -23,3 +25,21 @@ class Moto(models.Model):
     class Meta:
         verbose_name = 'мотоцикл'
         verbose_name_plural = 'мотоциклы'
+
+
+class Milage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, **NULLABLE, related_name='milage_car',
+                            verbose_name='машина')
+    moto = models.ForeignKey(Moto, on_delete=models.CASCADE, **NULLABLE, related_name='milage_moto',
+                             verbose_name='мотоцикл')
+
+    milage = models.PositiveIntegerField(verbose_name='пробег')
+    year = models.SmallIntegerField(verbose_name='год регистрации')
+
+    def __str__(self):
+        return f'{self.moto if self.moto else self.car} - {self.year}'
+
+    class Meta:
+        verbose_name = 'пробег'
+        verbose_name_plural = 'пробег'
+        ordering = ('-year',)
